3804267b90a2d76daa80b34e37d3c1ff02864af7
a22marca <128985931+a22marca@users.noreply.github.com>
2023-04-18 13:21:50 +0200

    Implementerat en onclick metod och onClickListener för Knappen i applikationen

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.buttonView);
+        image = findViewById(R.id.imageView);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(image.isShown()) {
+                    image.setVisibility(View.INVISIBLE);
+                }else{
+                    image.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
